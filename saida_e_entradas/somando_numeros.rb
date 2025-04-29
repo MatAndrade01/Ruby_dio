e44c0111a48870fe385f238c5e13aa11ea2edbd5
MatAndrade01 <ltbmatheus141012@gmail.com>
2025-04-29 00:04:56 -0300

    Aprendendo ruby e fazendo o primeiro desafio

---
+++ saida_e_entradas/somando_numeros.rb
@@ -0,0 +1,7 @@
+puts "Digite um número: "
+numero1 = gets.chomp.to_i
+puts "Digite outro numero: "
+numero2 = gets.chomp.to_i
+soma = numero1 + numero2
+
+puts "A soma entre #{numero1} e #{numero2} será #{soma}"
