f21a7d312a792f01f931f3133319e85b20de42fb
MatAndrade01 <ltbmatheus141012@gmail.com>
2025-04-29 00:13:04 -0300

    Aprendendo condicionais em ruby

---
+++ condicionais/elsif.rb
@@ -0,0 +1,11 @@
+dia = "Domingo"
+
+if dia == "Domingo"
+  almoco = "especial"
+elsif dia == "Feriado"
+  almoco = "mais tarde"
+else
+  almoco = "normal"
+end
+
+puts "Hoje nosso almoço será #{almoco}"
---
+++ condicionais/if_else.rb
@@ -0,0 +1,8 @@
+dia = "Segunda"
+
+if dia == "Domingo"
+  almoco = "especial"
+else
+  almoco = "normal"
+end
+puts "Nosso almoço será #{almoco}"
